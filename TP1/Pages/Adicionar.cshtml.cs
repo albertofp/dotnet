c266a3b625390e70fca4c0760e19e18cac28ef53
albertofp <albertopluecker@gmail.com>
2023-11-12 22:38:09 +0100

    add onpost

--- TP1/Pages/Adicionar.cshtml.cs
+++ TP1/Pages/Adicionar.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using TP1.Models;
 
@@ -5,9 +6,16 @@ namespace TP1.Pages;
 
 public class Adicionar : PageModel
 {
+    [BindProperty]
     public Book Book { get; set; }
 
-    public void OnGet()
+    public IActionResult OnPost()
     {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        return RedirectToPage("Index");
     }
 }
