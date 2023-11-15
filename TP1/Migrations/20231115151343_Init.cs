e79e2ebb457b841c2703daaf16ee3a3d6f87f5db
albertofp <albertopluecker@gmail.com>
2023-11-15 16:31:10 +0100

    add database

---
+++ TP1/Migrations/20231115151343_Init.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TP1.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Genres",
+                columns: table => new
+                {
+                    GenreID = table.Column<string>(type: "TEXT", nullable: false),
+                    GenreString = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genres", x => x.GenreID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Author = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    ImageUri = table.Column<string>(type: "TEXT", nullable: false),
+                    Price = table.Column<double>(type: "REAL", nullable: false),
+                    FreeDelivery = table.Column<bool>(type: "INTEGER", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    GenreID = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Books_Genres_GenreID",
+                        column: x => x.GenreID,
+                        principalTable: "Genres",
+                        principalColumn: "GenreID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_GenreID",
+                table: "Books",
+                column: "GenreID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Genres");
+        }
+    }
+}
--- TP1/Models/Book.cs
+++ TP1/Models/Book.cs
@@ -1,40 +1,70 @@
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace TP1.Models;
 
 public class Book
 {
     public string Id { get; set; }
-    
+
     [Display(Name = "Título")]
     [Required(AllowEmptyStrings = false, ErrorMessage = "Campo 'Título' obrigatório.")]
     [StringLength(50, MinimumLength = 3, ErrorMessage = "Título deve conter entre 3 e 50 caracteres.")]
     public string Name { get; set; }
 
     public string NameSlug => Name.ToLower().Replace(" ", "-");
-    
-    [Required(AllowEmptyStrings = false,ErrorMessage = "Campo 'Autor' obrigatório.")]
+
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Campo 'Autor' obrigatório.")]
     [StringLength(50, MinimumLength = 3, ErrorMessage = "Autor deve conter entre 3 e 50 caracteres.")]
     [Display(Name = "Autor")]
     public string Author { get; set; }
-    
+
     [Required(ErrorMessage = "Campo 'Link Capa' obrigatório.")]
     [Display(Name = "Link Capa")]
     public string ImageUri { get; set; }
-   
+
     [Display(Name = "Preço")]
     [Required(ErrorMessage = "Campo 'Preço' obrigatório.")]
     [DataType(DataType.Currency)]
     public double Price { get; set; }
-    
-    [Display(Name = "Entrega grátis")]
-    public bool FreeDelivery { get; set; }
+
+    [Display(Name = "Entrega grátis")] public bool FreeDelivery { get; set; }
     public string FreeDeliveryDisplay => FreeDelivery ? "Sim" : "Não";
-    
+
     [DisplayFormat(DataFormatString = "{0:MMMM \\de yyyy}")]
     [Required(ErrorMessage = "Campo 'Disponível em' obrigatório.")]
     [DataType("month", ErrorMessage = "Obrigatório")]
     [Display(Name = "Disponível em")]
     public DateTime CreationDate { get; set; }
+
+    public string GenreID { get; set; }
+    public Genre Genre { get; set; }
+}
+
+public class BooksContext : DbContext
+{
+    public DbSet<Book> Books { get; set; }
+    public DbSet<Genre> Genres { get; set; }
+    public string DbPath { get; }
+
+    public BooksContext(DbSet<Book> books, DbSet<Genre> genres)
+    {
+        Books = books;
+        Genres = genres;
+        DbPath = "books.db";
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlite($"Data Source={DbPath}");
+    }
+}
+
+public class Genre
+{
+    public List<Book> Books { get; } = new();
+
+    public string GenreString { get; set; }
+    public string GenreID { get; set; }
 }
